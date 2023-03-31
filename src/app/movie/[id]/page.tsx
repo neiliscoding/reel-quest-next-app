5c356d52104289d2c7ed4042bf27cbd474027fe0
Neil <neil.coder@gmail.com>
2023-03-31 19:03:29 +0200

    Update page.tsx
    getting values now

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react';
+// import type { FC } from 'react';
 
 const API_KEY = process.env.API_KEY;
 
@@ -9,29 +9,44 @@ interface pageProps {
   searchParams: {};
 }
 
-const page: FC<pageProps> = (params) => {
+export default async function MoviePage(params: pageProps) {
   console.log(params);
   
   const movieId = params.params.id;
   const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`;
   console.log(url);
-  let movie
+  let movie;
 
-  fetch(url)
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const data = await res.json();
+  
+  console.log('data ' + data);
+
+
+/*   fetch(url)
     .then(response => response.json())
     .then(data => {
       movie = data;
       console.log(movie);
-      return (<div>{movie}</div>);
+      // return (<div>{movie}</div>);
     })
     .catch(error => {
       console.error(error);
-      return (<div>error</div>);
+      // return (<div>error</div>);
     });
-  
-    return (<div>nothing</div>);
+   */
+
+          return (<div>
+            <h2>{data.original_title}</h2><h3>{data.overview}</h3></div>);
+
+    // return (<div>nothing</div>);
   // return (<div>{
   //     movie? ( <div>{movie}</div>) : (<div>Loading...</div>)
   //   }</div>);
 }
-export default page;
+// export default page;
