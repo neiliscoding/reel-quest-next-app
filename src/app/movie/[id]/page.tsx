7028219e0be5385a29267f20c8735a62517f2a98
Neil <neil.coder@gmail.com>
2023-03-31 15:42:27 +0200

    Update page.tsx
    trying to get the movie page work

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,23 +1,37 @@
 import type { FC } from 'react';
 
+const API_KEY = process.env.API_KEY;
+
 interface pageProps {
-  id: string;
+  params: {
+    id: string,
+  }
+  searchParams: {};
 }
 
 const page: FC<pageProps> = (params) => {
-  const movieId = params.id;
-  const url = `https://api.themoviedb.org/3/movie/${movieId}?api-key=${process.env.API_KEY}&language=en-US`;
-  let movie;
+  console.log(params);
+  
+  const movieId = params.params.id;
+  const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`;
+  console.log(url);
+  let movie
+
   fetch(url)
     .then(response => response.json())
     .then(data => {
       movie = data;
+      console.log(movie);
+      return (<div>{movie}</div>);
     })
     .catch(error => {
       console.error(error);
+      return (<div>error</div>);
     });
-  return (<div>
-    <h2>Movie</h2>
-  </div>);
+  
+    return (<div>nothing</div>);
+  // return (<div>{
+  //     movie? ( <div>{movie}</div>) : (<div>Loading...</div>)
+  //   }</div>);
 }
 export default page;
